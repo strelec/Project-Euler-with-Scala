b26db07947ba8d794d912494466fb376734aaa13
Rok Kralj <info@rok-kralj.net>
2016-02-27 22:49:10 +0100

    391: Figured out a pretty simple solution.

--- 381.scala
+++ 381.scala
@@ -1,21 +1,6 @@
-import helpers._
+val N = 100000000
+val sieve = helpers.Sieve(N)
 
-val sieve = Sieve(100)
-
-@inline
-def t(a: Long, b: Long, mod: Int) =
-	a * b % mod
-
-@inline
-def factmod(n: Int, mod: Int): Long =
-	(2L to n).foldLeft(0L)(_ * _ % mod)
-
-
-
-val result = sieve.primesIter().drop(2).map { p =>
-	val q = p.toLong - 1
-	//factmod(p-5, p) * (q * q % p * (q-4) + 1) % p * (q - 3) % p
-	t(t(t(t(q, q, p), q-2, p) + 1, q-3, p) + 1, factmod(p-5, p), p)
-}.sum
-
-println(result)
+println(sieve.primesIter(5).map( p =>
+	(p.toLong*p - 1)/8 * 3 % p
+).sum)
