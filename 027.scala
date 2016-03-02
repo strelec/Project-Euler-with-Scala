ef1b6f76d956b675ad720a9c8302178903d727e3
Rok Kralj <info@rok-kralj.net>
2016-03-02 20:20:41 +0100

    27: Syntax cleanup.

--- 027.scala
+++ 027.scala
@@ -1,15 +1,12 @@
-import helpers._
+val sieve = helpers.Sieve(20000)
+val	primes = sieve.primes
 
-val sieve = Sieve(20000)
-val stream = sieve.primes()
+val result = for {
+	b <- primes.takeWhile(_ < 1000)
+	a <- primes.map(_ - b - 1).takeWhile(_ < 1000)
+	c = Iterator.from(0).map( n =>
+		(n*n + a*n + b).toLong
+	).takeWhile(sieve.isPrime).size
+} yield (c, a * b)
 
-val (c, a, b) = (for {
-	b <- stream.takeWhile(_ < 1000)
-	a <- stream.map(_ - b - 1).takeWhile(_ < 1000)
-} yield {
-	def eq(n: Int) = n*n + a*n + b
-	val common = Stream.from(0).map(eq).takeWhile(sieve.isPrime).size
-	(common, a, b)
-}).max
-
-println(a*b)
+println(result.max._2)
