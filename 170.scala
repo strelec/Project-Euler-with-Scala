345db7c846178ed99ce5c65e3697ae5eb617fd49
Rok Kralj <info@rok-kralj.net>
2016-03-02 17:57:40 +0100

    170: Migrate to Sieve#divisorsOf

--- 170.scala
+++ 170.scala
@@ -1,6 +1,4 @@
-val factoring = Vector.tabulate(13000)(
-	helpers.NativeFactoring(_).divisors.tail
-)
+val sieve = helpers.Sieve(200)
 
 val result = "9876543210".permutations.filter( perm =>
 	(1 to 9).exists( i =>
@@ -8,12 +6,12 @@ val result = "9876543210".permutations.filter( perm =>
 			val a = perm.take(i).toInt
 			val b = perm.drop(i).toInt
 			
-			factoring(helpers.Helpers.gcd(a, b)).exists { c =>
+			sieve.divisorsOf(helpers.Helpers.gcd(a, b)).exists { c =>
 				val s = c.toString + (a/c).toString + (b/c).toString
 				s.size == 10 && s.distinct.size == 10
 			}
 		}
 	)
-).next
+)
 
-println(result)
+println(result.next)
