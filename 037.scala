b8a0f4ecfd7f7e10e2780915a0d122721152f0f8
Rok Kralj <info@rok-kralj.net>
2016-03-02 18:57:47 +0100

    37: Added the check for prime sieve size.

--- 037.scala
+++ 037.scala
@@ -1,11 +1,10 @@
-import helpers._
+val sieve = helpers.Sieve(800000)
 
-val sieve = Sieve(1000000)
-
-val result = sieve.primes.dropWhile(_ < 10).filter { p =>
+val result = sieve.primesIter(10).filter { p =>
 	val str = p.toString
 	val subs = str.inits.slice(1, str.size) ++ str.tails.slice(1, str.size)
 	subs.forall(x => sieve.isPrime(x.toLong))
-}
+}.take(11).toList
 
+require(result.size == 11, "The sieve is not big enough.")
 println(result.sum)
