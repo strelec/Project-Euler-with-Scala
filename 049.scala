8ed54873afb1869ecc2c6960bd8ec214b76dcfa7
Rok Kralj <info@rok-kralj.net>
2016-03-02 19:33:10 +0100

    49: Huge cleanup of one of my first Scala solutions.

--- 049.scala
+++ 049.scala
@@ -1,25 +1,12 @@
-def primes(n: Int) = {
-	val nums = 2 to n
-	val out = mutable.BitSet(nums: _*)
-	for (p <- nums.takeWhile(x => x*x <= n) if out(p))
-		out --= p * p to n by p
-	out
-}
+val buckets = helpers.Sieve(10000).primesIter(1000).toSeq.groupBy(_.toString.sorted)
 
-var buckets = Map.empty[Seq[Char], IndexedSeq[Int]].withDefaultValue(IndexedSeq())
+val result = for {
+	(key, vals) <- buckets
+	i <- 0 until vals.size
+	if vals(i) != 1487
+	j <- i+1 until vals.size
+	third = 2*vals(j) - vals(i)
+	if vals contains third
+} yield "" + vals(i) + vals(j) + third
 
-primes(9999).dropWhile(_ < 1000).foreach { p =>
-	val s = p.toString.toSeq.sorted
-	buckets += s -> (buckets(s) :+ p)
-}
-
-val solution = buckets.flatMap { case (key, vals) =>
-	for {
-		i <- 0 until vals.size
-		j <- i+1 until vals.size
-		third = 2*vals(j) - vals(i)
-		if (vals contains third)
-	} yield (vals(i), vals(j), third)
-}.toSeq.diff(Seq((1487,4817,8147))).head
-
-println("" + solution._1 + solution._2 + solution._3)
+println(result.head)
