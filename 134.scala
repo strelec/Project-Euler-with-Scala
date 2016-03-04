4725dbb1035e4750bf2b952ba6338b2e00438cc1
Rok Kralj <info@rok-kralj.net>
2016-03-04 20:46:10 +0100

    134: Solution.

---
+++ 134.scala
@@ -0,0 +1,12 @@
+val N = 1000000
+val sieve = helpers.Sieve(N+200)
+val p10 = helpers.Number.powers(10).map(BigInt(_)).take(15).toVector
+
+val result = for {
+	Seq(a, b) <- sieve.primesIter(5).sliding(2)
+	if a <= N
+	y = p10(helpers.Number.digits(a) - 1)
+	r = y.modInverse(b).toLong * (b - a) % b
+} yield y*r + a
+
+println(result.sum)
