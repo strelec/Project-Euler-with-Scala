0f1975d2c3f17e6c71b8cd3b9c2da4ceda77deaa
Rok Kralj <info@rok-kralj.net>
2016-03-11 13:58:48 +0100

    487: Correct solution.

--- 487.scala
+++ 487.scala
@@ -1,12 +1,13 @@
 val N = 1000000000000L
 val K = 10000
 
-val P = 2000000000
-val sieve = helpers.Sieve(100000)
-val primes = (P.toLong to P+2000).filter(sieve.isPrime)
+val primes = {
+	val P = 2000000000
+	val sieve = helpers.Sieve(100000)
+	(P to P+2000).filter( p => sieve.isPrime(p.toInt) )
+}
 
-def f(n: Long, k: Int, p: Long) = {
-	//var b = (BigInt(n+k+1) until n by -1).reduce(_ * _ % p)
+def f(n: Int, k: Int, p: Int) = {
 	var b = (BigInt(n) until n-k-1 by -1).reduce(_ * _ % p)
 	(2 to k+1).foreach { i =>
 		b *= BigInt(i).modInverse(p)
@@ -20,32 +21,20 @@ def f(n: Long, k: Int, p: Long) = {
 	val binoms =
 		helpers.Helpers.binoms(k+1).map(_ % p).map(_.toLong).toArray
 
-	(k to 1 by -1).map { i =>
-	//(1 to k).map { i =>
-		//b *= n-i+1
-		b *= n+k-i+1
-		if (b != 0) {
-			//b *= BigInt(n-i+k+2).modInverse(p)
-			b *= BigInt(n-i).modInverse(p)
-			b %= p
-			
-			b * (0 to i-1).map( j =>
-				powers(i - j) * binoms(j) *
-				(if (j % 2 == 0) 1 else -1) % p
-			).reduce(_ + _ % p)
-		} else {
-			println("ZERO", n,i,k,p)
-			BigInt(0)
-		}
+	(k to math.max(1, n-p+k+1) by -1).map { i =>
+		b *= n-i+k+1
+		b *= BigInt(n-i).modInverse(p)
+		
+		b * (0 to i-1).map( j =>
+			powers(i - j) * binoms(j) *
+			(if (j % 2 == 0) 1 else -1) % p
+		).reduce(_ + _ % p) % p
 	}.sum % p
 }
 
-//println(f(100, 4, 2147483647))
-
-
-lazy val result = primes.take(3).par.map { p =>
-	val tmp = ((N+1)*f(N%p, K, p) - f(N%p, K+1, p)) % p
-	println(tmp)
+val result = primes.par.map { p =>
+	val n = (N % p).toInt
+	val tmp = ((n+1) * f(n, K, p) - f(n, K+1, p)) % p
 	if (tmp < 0) tmp + p else tmp
 }
 
