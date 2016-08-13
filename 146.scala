da3337f711a532986adc50cf3c96df233166df72
Rok Kralj <info@rok-kralj.net>
2016-08-13 17:04:12 +0100

    146: Shorten the code a bit.

--- 146.scala
+++ 146.scala
@@ -1,23 +1,17 @@
-val N = 150000000
+val N = 150000000 - 1
+
+def pp(n: BigInt) = n.isProbablePrime(5)
 
 val result = for {
-	a <- Seq(10L, 60L)
-	n <- a until N by 70
+	a <- Seq(10L, 80L, 130L, 200L)
+	n <- a to N by 210
 	
-	if n % 3 != 0
 	m = n % 13
 	if m == 1 || m == 3 || m == 4 || m == 9 || m == 10 || m == 12
 	
 	n2 = BigInt(n*n)
-	if (n2 + 1).isProbablePrime(5)
-	if (n2 + 3).isProbablePrime(5)
-	if (n2 + 7).isProbablePrime(5)
-	if (n2 + 9).isProbablePrime(5)
-	if (n2 + 13).isProbablePrime(5)
-	if (n2 + 27).isProbablePrime(5)
-	
-	if !(n2 + 19).isProbablePrime(5)
-	if !(n2 + 21).isProbablePrime(5)
+	if pp(n2 + 1) && pp(n2 + 3) && pp(n2 + 7) && pp(n2 + 9) && pp(n2 + 13) && pp(n2 + 27)
+	if !pp(n2 + 19) && !pp(n2 + 21)
 } yield n
 
 println(result.sum)
