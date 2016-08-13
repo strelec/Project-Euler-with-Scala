0db8e41a8b9fd58d005600612f76e12415a5d217
Rok Kralj <info@rok-kralj.net>
2016-08-13 16:53:00 +0100

    146: Solution.

--- 146.scala
+++ 146.scala
@@ -1,15 +1,24 @@
-import helpers._
+val N = 150000000
 
-val seq = Vector(1,3,7,9,13,27).reverse.map(_.toLong)
-
-val ns = for {
-	p <- Seq(10L, 20L)
-	n <- p until 15000000L by 30L
-
-	if seq.forall{ i =>
-		println(s"FAKTOR $n² + $i")
-		SysFactoring(n*n + i).isPrime
-	}
+val result = for {
+	k <- 0L to N/70
+	n <- Seq(70*k + 10, 70*k + 60)
+	if n < N
+	
+	if n % 3 != 0
+	m = n % 13
+	if m == 1 || m == 3 || m == 4 || m == 9 || m == 10 || m == 12
+	
+	n2 = BigInt(n*n)
+	if (n2 + 1).isProbablePrime(5)
+	if (n2 + 3).isProbablePrime(5)
+	if (n2 + 7).isProbablePrime(5)
+	if (n2 + 9).isProbablePrime(5)
+	if (n2 + 13).isProbablePrime(5)
+	if (n2 + 27).isProbablePrime(5)
+	
+	if !(n2 + 19).isProbablePrime(5)
+	if !(n2 + 21).isProbablePrime(5)
 } yield n
 
-println(ns.sum)
+println(result.sum)
