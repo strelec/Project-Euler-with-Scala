dc22bb864bce7c04e9d332983293463083cad2c1
Rok Kralj <info@rok-kralj.net>
2016-08-21 19:57:25 +0100

    52: More idiomatic solution.

--- 052.scala
+++ 052.scala
@@ -1,7 +1,7 @@
-val result = (Stream from 1).filter { i =>
-	math.log10(i) % 1 < math.log10(10/3)
-}.filter { i =>
-	(2 to 6).map(x => (x * i).toString.sorted).distinct.size == 1
-}
+val result = for {
+	i <- Iterator.from(1)
+	if math.log10(i) % 1 < math.log10(10.0/3)
+	if (2 to 6).map(_ * i).map(_.toString.sorted).distinct.size == 1
+} yield i
 
-println(result.head)
+println(result.next)
