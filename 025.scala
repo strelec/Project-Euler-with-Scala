bed4a52f415194afb8caed6bbb31c0fcc8eebd0b
Rok Kralj <info@rok-kralj.net>
2016-08-26 00:27:55 +0100

    25: Massively improve the code style.

--- 025.scala
+++ 025.scala
@@ -1,5 +1,10 @@
+val N = 1000
+
 def fib(a: BigInt = 1, b: BigInt = 1): Stream[BigInt] = a #:: fib(b, a+b)
 
-val result = fib().zipWithIndex.find(_._1.toString.size >= 1000)
+val result #:: _ = for {
+	(n, i) <- fib().zipWithIndex
+	if n.toString.size >= N
+} yield i + 1
 
-println(result.get._2 + 1)
+println(result)
