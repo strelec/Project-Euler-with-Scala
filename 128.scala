4cd399958703c92ac95e087fb443547ac7e922fd
Rok Kralj <info@rok-kralj.net>
2016-09-17 12:10:01 +0200

    128: Improve solution.

--- 128.scala
+++ 128.scala
@@ -1,16 +1,14 @@
 val N = 2000
 
-def f(n: Int, p: Int) =
-	3L*n*(n+1) + p
-
 val gen = for {
 	n <- Iterator.from(1)
+	if BigInt(6*n + 5).isProbablePrime(5)
+	s = 3L * n * (n+1)
 	(t, a) <- Seq(
-		f(n,1) -> Seq(f(n-2,2), /*f(n-1,1),*/ f(n-1,2), /*f(n,0),*/ f(n+1,0)/*, f(n+1,1)*/),
-		f(n,2) -> Seq(/*f(n-1,2),*/ /*f(n,3),*/ f(n+1,1), /*f(n+1,2),*/ f(n+1,3), f(n+2,1))
+		(s + 1) -> Seq(6*n - 1, 12*n - 7),
+		(s + 2) -> Seq(6*n + 7, 12*n + 17)
 	)
-	diff = a.map(_ - t).map(math.abs).map(BigInt(_))
-	if diff.count(_.isProbablePrime(5)) == 3
-} yield t -> diff
+	if a.forall(BigInt(_).isProbablePrime(5))
+} yield t
 
-println(gen.drop(N-2).next._1)
+println(gen.drop(N-2).next)
